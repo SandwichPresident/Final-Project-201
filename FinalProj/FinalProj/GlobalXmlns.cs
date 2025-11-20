34e5f4cfb77195b686363fd0164cf81ea557f66f
SandwichPresident <shb1713@rit.edu>
2025-11-20 08:29:04 -0500

    Created Maui App

---
+++ FinalProj/FinalProj/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FinalProj")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FinalProj.Pages")]

c4b2ab35545ab24ff464b5266fefc7e13eee4156
kaylahr-28 <kaylahrouzier28@gmail.com>
2025-12-02 08:11:10 -0500

    deleting and adding new project template..

--- FinalProj/FinalProj/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FinalProj")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FinalProj.Pages")]

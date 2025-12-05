4c6e2c68dbb189f7a0ee822e24905853509ddc95
kaylahr-28 <kaylahrouzier28@gmail.com>
2025-12-05 14:23:59 -0500

    adjusting grid on drink page, more styling

--- FinalProject201/DrinkPage.xaml.cs
+++ FinalProject201/DrinkPage.xaml.cs
@@ -7,5 +7,32 @@
             InitializeComponent();
         }
 
+        void StepperValChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            //right now only first stepper works </3
+          if (sender is Stepper stepper)
+            {
+                if (stepper.Equals(colaStep))
+                {
+                    UpdateBtnTxt(colaBtn, value);
+                  
+                } else if (stepper.Equals(waterStep))
+                {
+                    UpdateBtnTxt(waterBtn, value);
+                } else if (stepper.Equals(coffeeStep))
+                {
+                    UpdateBtnTxt(colaBtn, value);
+                }
+            } 
+          
+        }
+
+        void UpdateBtnTxt(Button button, double val)
+        {
+            button.Text = string.Format("Add {0} to cart", val);
+
+        }
+
     }
 }
